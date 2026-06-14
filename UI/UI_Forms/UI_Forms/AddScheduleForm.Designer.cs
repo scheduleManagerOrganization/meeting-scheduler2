11081f0e3267a8f9017f86c2cf350bf44ccacfa3
yoon0x5a <yoongy3512@gmail.com>
2026-06-15 01:47:37 +0900

    반복일정 추가기능 추가

--- UI/UI_Forms/UI_Forms/AddScheduleForm.Designer.cs
+++ UI/UI_Forms/UI_Forms/AddScheduleForm.Designer.cs
@@ -15,7 +15,16 @@
         private System.Windows.Forms.ComboBox cmbEndHour;
         private System.Windows.Forms.Label lblEndColon;
         private System.Windows.Forms.ComboBox cmbEndMinute;
-        // 색상 관련 UI 선언 삭제
+
+        // 🌟 추가된 반복 관련 컨트롤
+        private System.Windows.Forms.CheckBox chkRecurrence;
+        private System.Windows.Forms.Panel pnlRecurrence;
+        private System.Windows.Forms.ComboBox cmbRecurCount;
+        private System.Windows.Forms.Label lblRecurEvery;
+        private System.Windows.Forms.ComboBox cmbRecurType;
+        private System.Windows.Forms.Label lblRecurEnd;
+        private System.Windows.Forms.DateTimePicker dtpRecurEnd;
+
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
 
@@ -39,8 +48,16 @@
             this.cmbEndHour = new System.Windows.Forms.ComboBox();
             this.lblEndColon = new System.Windows.Forms.Label();
             this.cmbEndMinute = new System.Windows.Forms.ComboBox();
+            this.chkRecurrence = new System.Windows.Forms.CheckBox();
+            this.pnlRecurrence = new System.Windows.Forms.Panel();
+            this.cmbRecurCount = new System.Windows.Forms.ComboBox();
+            this.cmbRecurType = new System.Windows.Forms.ComboBox();
+            this.lblRecurEvery = new System.Windows.Forms.Label();
+            this.lblRecurEnd = new System.Windows.Forms.Label();
+            this.dtpRecurEnd = new System.Windows.Forms.DateTimePicker();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.pnlRecurrence.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblHeader
@@ -49,7 +66,8 @@
             this.lblHeader.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
             this.lblHeader.Location = new System.Drawing.Point(20, 20);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(126, 25);
+            this.lblHeader.Size = new System.Drawing.Size(95, 25);
+            this.lblHeader.TabIndex = 25;
             this.lblHeader.Text = "일정 추가";
             // 
             // txtTitle
@@ -58,72 +76,159 @@
             this.txtTitle.Location = new System.Drawing.Point(25, 60);
             this.txtTitle.Name = "txtTitle";
             this.txtTitle.Size = new System.Drawing.Size(285, 27);
+            this.txtTitle.TabIndex = 24;
             // 
             // lblStart
             // 
             this.lblStart.AutoSize = true;
             this.lblStart.Location = new System.Drawing.Point(22, 108);
-            this.lblStart.Size = new System.Drawing.Size(31, 15);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(29, 12);
+            this.lblStart.TabIndex = 23;
             this.lblStart.Text = "시작";
             // 
             // dtpStartDate
             // 
             this.dtpStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtpStartDate.Location = new System.Drawing.Point(60, 105);
-            this.dtpStartDate.Size = new System.Drawing.Size(125, 23);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(125, 21);
+            this.dtpStartDate.TabIndex = 22;
             // 
             // cmbStartHour
             // 
             this.cmbStartHour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbStartHour.Location = new System.Drawing.Point(195, 105);
-            this.cmbStartHour.Size = new System.Drawing.Size(48, 23);
+            this.cmbStartHour.Name = "cmbStartHour";
+            this.cmbStartHour.Size = new System.Drawing.Size(48, 20);
+            this.cmbStartHour.TabIndex = 21;
             // 
             // lblStartColon
             // 
             this.lblStartColon.AutoSize = true;
             this.lblStartColon.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
             this.lblStartColon.Location = new System.Drawing.Point(245, 107);
-            this.lblStartColon.Size = new System.Drawing.Size(12, 19);
+            this.lblStartColon.Name = "lblStartColon";
+            this.lblStartColon.Size = new System.Drawing.Size(13, 19);
+            this.lblStartColon.TabIndex = 20;
             this.lblStartColon.Text = ":";
             // 
             // cmbStartMinute
             // 
             this.cmbStartMinute.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbStartMinute.Location = new System.Drawing.Point(260, 105);
-            this.cmbStartMinute.Size = new System.Drawing.Size(48, 23);
+            this.cmbStartMinute.Name = "cmbStartMinute";
+            this.cmbStartMinute.Size = new System.Drawing.Size(48, 20);
+            this.cmbStartMinute.TabIndex = 19;
             // 
             // lblEnd
             // 
             this.lblEnd.AutoSize = true;
             this.lblEnd.Location = new System.Drawing.Point(22, 148);
-            this.lblEnd.Size = new System.Drawing.Size(31, 15);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(29, 12);
+            this.lblEnd.TabIndex = 18;
             this.lblEnd.Text = "종료";
             // 
             // dtpEndDate
             // 
             this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtpEndDate.Location = new System.Drawing.Point(60, 145);
-            this.dtpEndDate.Size = new System.Drawing.Size(125, 23);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(125, 21);
+            this.dtpEndDate.TabIndex = 17;
             // 
             // cmbEndHour
             // 
             this.cmbEndHour.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbEndHour.Location = new System.Drawing.Point(195, 145);
-            this.cmbEndHour.Size = new System.Drawing.Size(48, 23);
+            this.cmbEndHour.Name = "cmbEndHour";
+            this.cmbEndHour.Size = new System.Drawing.Size(48, 20);
+            this.cmbEndHour.TabIndex = 16;
             // 
             // lblEndColon
             // 
             this.lblEndColon.AutoSize = true;
             this.lblEndColon.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Bold);
             this.lblEndColon.Location = new System.Drawing.Point(245, 147);
-            this.lblEndColon.Size = new System.Drawing.Size(12, 19);
+            this.lblEndColon.Name = "lblEndColon";
+            this.lblEndColon.Size = new System.Drawing.Size(13, 19);
+            this.lblEndColon.TabIndex = 15;
             this.lblEndColon.Text = ":";
             // 
             // cmbEndMinute
             // 
             this.cmbEndMinute.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbEndMinute.Location = new System.Drawing.Point(260, 145);
-            this.cmbEndMinute.Size = new System.Drawing.Size(48, 23);
+            this.cmbEndMinute.Name = "cmbEndMinute";
+            this.cmbEndMinute.Size = new System.Drawing.Size(48, 20);
+            this.cmbEndMinute.TabIndex = 14;
+            // 
+            // chkRecurrence
+            // 
+            this.chkRecurrence.AutoSize = true;
+            this.chkRecurrence.Location = new System.Drawing.Point(25, 185);
+            this.chkRecurrence.Name = "chkRecurrence";
+            this.chkRecurrence.Size = new System.Drawing.Size(76, 16);
+            this.chkRecurrence.TabIndex = 10;
+            this.chkRecurrence.Text = "반복 일정";
+            // 
+            // pnlRecurrence
+            // 
+            this.pnlRecurrence.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlRecurrence.Controls.Add(this.cmbRecurCount);
+            this.pnlRecurrence.Controls.Add(this.cmbRecurType);
+            this.pnlRecurrence.Controls.Add(this.lblRecurEvery);
+            this.pnlRecurrence.Controls.Add(this.lblRecurEnd);
+            this.pnlRecurrence.Controls.Add(this.dtpRecurEnd);
+            this.pnlRecurrence.Location = new System.Drawing.Point(25, 210);
+            this.pnlRecurrence.Name = "pnlRecurrence";
+            this.pnlRecurrence.Size = new System.Drawing.Size(285, 65);
+            this.pnlRecurrence.TabIndex = 11;
+            this.pnlRecurrence.Visible = false;
+            // 
+            // cmbRecurCount
+            // 
+            this.cmbRecurCount.FormattingEnabled = true;
+            this.cmbRecurCount.Location = new System.Drawing.Point(10, 8);
+            this.cmbRecurCount.Name = "cmbRecurCount";
+            this.cmbRecurCount.Size = new System.Drawing.Size(45, 20);
+            this.cmbRecurCount.TabIndex = 0;
+            // 
+            // cmbRecurType
+            // 
+            this.cmbRecurType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRecurType.FormattingEnabled = true;
+            this.cmbRecurType.Location = new System.Drawing.Point(60, 8);
+            this.cmbRecurType.Name = "cmbRecurType";
+            this.cmbRecurType.Size = new System.Drawing.Size(45, 20);
+            this.cmbRecurType.TabIndex = 1;
+            // 
+            // lblRecurEvery
+            // 
+            this.lblRecurEvery.AutoSize = true;
+            this.lblRecurEvery.Location = new System.Drawing.Point(110, 11);
+            this.lblRecurEvery.Name = "lblRecurEvery";
+            this.lblRecurEvery.Size = new System.Drawing.Size(29, 12);
+            this.lblRecurEvery.TabIndex = 2;
+            this.lblRecurEvery.Text = "마다";
+            // 
+            // lblRecurEnd
+            // 
+            this.lblRecurEnd.AutoSize = true;
+            this.lblRecurEnd.Location = new System.Drawing.Point(8, 38);
+            this.lblRecurEnd.Name = "lblRecurEnd";
+            this.lblRecurEnd.Size = new System.Drawing.Size(41, 12);
+            this.lblRecurEnd.TabIndex = 3;
+            this.lblRecurEnd.Text = "종료일";
+            // 
+            // dtpRecurEnd
+            // 
+            this.dtpRecurEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpRecurEnd.Location = new System.Drawing.Point(60, 34);
+            this.dtpRecurEnd.Name = "dtpRecurEnd";
+            this.dtpRecurEnd.Size = new System.Drawing.Size(115, 21);
+            this.dtpRecurEnd.TabIndex = 4;
             // 
             // btnSave
             // 
@@ -131,24 +236,31 @@
             this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSave.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold);
             this.btnSave.ForeColor = System.Drawing.Color.White;
-            this.btnSave.Location = new System.Drawing.Point(145, 190); // Y좌표 위로 당김
+            this.btnSave.Location = new System.Drawing.Point(145, 190);
+            this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 35);
+            this.btnSave.TabIndex = 13;
             this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
             this.btnCancel.BackColor = System.Drawing.Color.LightGray;
             this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCancel.Location = new System.Drawing.Point(235, 190); // Y좌표 위로 당김
+            this.btnCancel.Location = new System.Drawing.Point(235, 190);
+            this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 35);
+            this.btnCancel.TabIndex = 12;
             this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = false;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // AddScheduleForm
             // 
-            // 높이를 줄임 (305 -> 240)
             this.ClientSize = new System.Drawing.Size(340, 240);
+            this.Controls.Add(this.pnlRecurrence);
+            this.Controls.Add(this.chkRecurrence);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.cmbEndMinute);
@@ -169,8 +281,11 @@
             this.Name = "AddScheduleForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "일정 추가";
+            this.pnlRecurrence.ResumeLayout(false);
+            this.pnlRecurrence.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
     }
 }
--- UI/UI_Forms/UI_Forms/AddScheduleForm.cs
+++ UI/UI_Forms/UI_Forms/AddScheduleForm.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using UI_Forms.Models;
+using UI_Forms.Models; // 기존 네임스페이스 유지
 
 namespace UI_Forms
 {
@@ -15,6 +17,7 @@ namespace UI_Forms
             InitializeComponent();
             SetupTimeComboBoxes();
             SetupPlaceholder();
+            SetupRecurrenceControls(); // 반복 컨트롤 초기화 추가
 
             dtpStartDate.Value = DateTime.Today;
             dtpEndDate.Value = DateTime.Today;
@@ -42,7 +45,6 @@ namespace UI_Forms
             };
         }
 
-        // 🌟 시간과 분을 따로 세팅하는 메서드 (5분 단위로 수정)
         private void SetupTimeComboBoxes()
         {
             cmbStartHour.Items.Clear();
@@ -57,7 +59,6 @@ namespace UI_Forms
                 cmbEndHour.Items.Add(hourStr);
             }
 
-            // 분: 00, 05, 10, 15 ... 55 (5분 단위로 변경)
             for (int m = 0; m < 60; m += 5)
             {
                 string minuteStr = $"{m:D2}";
@@ -65,10 +66,49 @@ namespace UI_Forms
                 cmbEndMinute.Items.Add(minuteStr);
             }
 
-            cmbStartHour.SelectedIndex = 9;   // 09시
-            cmbStartMinute.SelectedIndex = 0; // 00분
-            cmbEndHour.SelectedIndex = 10;    // 10시
-            cmbEndMinute.SelectedIndex = 0;   // 00분
+            cmbStartHour.SelectedIndex = 9;
+            cmbStartMinute.SelectedIndex = 0;
+            cmbEndHour.SelectedIndex = 10;
+            cmbEndMinute.SelectedIndex = 0;
+        }
+
+        // 🌟 반복 컨트롤 초기화 로직 추가
+        private void SetupRecurrenceControls()
+        {
+            cmbRecurType.Items.AddRange(new string[] { "일", "주", "월", "년" });
+            cmbRecurType.SelectedIndex = 1; // 기본값 "주"
+
+            for (int i = 1; i <= 10; i++)
+            {
+                cmbRecurCount.Items.Add(i.ToString());
+            }
+            cmbRecurCount.SelectedIndex = 0; // 기본값 "1"
+
+            dtpRecurEnd.Value = DateTime.Today.AddMonths(1);
+
+            chkRecurrence.CheckedChanged += (s, e) =>
+            {
+                if (chkRecurrence.Checked)
+                {
+                    this.ClientSize = new Size(340, 335); // 폼 확장
+                    pnlRecurrence.Visible = true;
+                    btnSave.Top = 285;
+                    btnCancel.Top = 285;
+                }
+                else
+                {
+                    pnlRecurrence.Visible = false;
+                    this.ClientSize = new Size(340, 240); // 폼 축소
+                    btnSave.Top = 190;
+                    btnCancel.Top = 190;
+                }
+            };
+
+            // 숫자 외 입력 방지
+            cmbRecurCount.KeyPress += (s, e) =>
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true;
+            };
         }
 
         private async void btnSave_Click(object sender, EventArgs e)
@@ -91,43 +131,107 @@ namespace UI_Forms
                 return;
             }
 
+            // 🌟 반복 날짜(일정 블록) 계산 로직
+            List<(DateTime start, DateTime end)> scheduleBlocks = new List<(DateTime, DateTime)>();
+
+            if (chkRecurrence.Checked)
+            {
+                if (!int.TryParse(cmbRecurCount.Text, out int interval) || interval <= 0)
+                {
+                    MessageBox.Show("반복 주기를 올바른 숫자로 입력해주세요.");
+                    return;
+                }
+
+                string recurType = cmbRecurType.SelectedItem.ToString();
+                DateTime recurEndLimit = dtpRecurEnd.Value.Date.AddDays(1).AddTicks(-1);
+
+                DateTime currentStart = startDateTime;
+                DateTime currentEnd = endDateTime;
+
+                while (currentStart <= recurEndLimit)
+                {
+                    scheduleBlocks.Add((currentStart, currentEnd));
+
+                    switch (recurType)
+                    {
+                        case "일": currentStart = currentStart.AddDays(interval); currentEnd = currentEnd.AddDays(interval); break;
+                        case "주": currentStart = currentStart.AddDays(interval * 7); currentEnd = currentEnd.AddDays(interval * 7); break;
+                        case "월": currentStart = currentStart.AddMonths(interval); currentEnd = currentEnd.AddMonths(interval); break;
+                        case "연": currentStart = currentStart.AddYears(interval); currentEnd = currentEnd.AddYears(interval); break;
+                    }
+                }
+            }
+            else
+            {
+                scheduleBlocks.Add((startDateTime, endDateTime));
+            }
+
             btnSave.Enabled = false;
             try
             {
-                bool allSuccess = true;
-                DateTime current = startDateTime.Date;
-                DateTime endLimit = endDateTime.Date;
+                List<Task<bool>> saveTasks = new List<Task<bool>>();
 
-                while (current <= endLimit)
+                // 기존의 자정 쪼개기 로직을 유지하면서 Task 리스트에 담기
+                foreach (var block in scheduleBlocks)
                 {
-                    string slotStart = (current == startDateTime.Date) ? startSlot : "00:00";
-                    string slotEnd = (current == endLimit) ? endSlot : "23:59";
+                    DateTime current = block.start.Date;
+                    DateTime endLimit = block.end.Date;
 
-                    if (slotStart == slotEnd) { current = current.AddDays(1); continue; }
-
-                    var request = new
+                    while (current <= endLimit)
                     {
-                        userId = ApiService.CurrentUserId,
-                        date = current.ToString("yyyy-MM-dd"),
-                        slots = new[] { new { title = txtTitle.Text, start = slotStart, end = slotEnd } }
-                    };
+                        string slotStart = (current == block.start.Date) ? block.start.ToString("HH:mm") : "00:00";
+                        string slotEnd = (current == endLimit) ? block.end.ToString("HH:mm") : "23:59";
 
-                    var response = await ApiService.PostAsync<object, ApiResponse<object>>("/api/availability", request);
-                    if (response == null || !response.Success) allSuccess = false;
+                        if (slotStart == slotEnd) { current = current.AddDays(1); continue; }
 
-                    current = current.AddDays(1);
+                        var request = new
+                        {
+                            user_id = ApiService.CurrentUserId, // 명세서에 맞춤 (user_id)
+                            date = current.ToString("yyyy-MM-dd"),
+                            slots = new[] { new { title = txtTitle.Text, start = slotStart, end = slotEnd } }
+                        };
+
+                        saveTasks.Add(SaveSingleSlotAsync(request));
+                        current = current.AddDays(1);
+                    }
                 }
 
-                if (allSuccess)
+                // 🌟 모든 API 요청을 일괄 대기
+                bool[] results = await Task.WhenAll(saveTasks);
+
+                if (results.Length > 0 && results.All(r => r == true))
                 {
-                    MessageBox.Show("일정이 등록되었습니다.", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("일정이 성공적으로 등록되었습니다.", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     this.DialogResult = DialogResult.OK;
                     this.Close();
                 }
-                else MessageBox.Show("일부 날짜 저장에 실패했습니다.");
+                else
+                {
+                    MessageBox.Show("일부 일정을 저장하는 데 실패했습니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
             }
-            catch (Exception ex) { MessageBox.Show("오류: " + ex.Message); }
-            finally { btnSave.Enabled = true; }
+            catch (Exception ex)
+            {
+                MessageBox.Show("오류: " + ex.Message);
+            }
+            finally
+            {
+                btnSave.Enabled = true;
+            }
+        }
+
+        // 개별 통신 헬퍼 메서드
+        private async Task<bool> SaveSingleSlotAsync(object request)
+        {
+            try
+            {
+                var response = await ApiService.PostAsync<object, ApiResponse<object>>("/api/availability", request);
+                return response != null && response.Success;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e) => this.Close();
